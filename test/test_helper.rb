e43c3995ebe7f9595acf8ebc0e37ad8c5b425770
Kelsey Krippaehne <kelseykripp@gmail.com>
2019-09-03 14:21:55 -0700

    Initiated simplecov

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,7 +2,11 @@
 require "minitest"
 require "minitest/autorun"
 require "minitest/reporters"
+require "simplecov"
+SimpleCov.start
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 # require_relative your lib files here!
+
+require_relative '../lib/reservation'
