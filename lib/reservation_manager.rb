8b1a5448e92168480c8d20528c1525679f94ec26
Kelsey Krippaehne <kelseykripp@gmail.com>
2019-09-04 21:52:30 -0700

    Added test & code for returning a list of reservations for a given date

--- lib/reservation_manager.rb
+++ lib/reservation_manager.rb
@@ -11,13 +11,21 @@ module Hotel
     end
     
     def create_reservation(start_date, end_date)
-      new_reservation = Hotel::Reservation.new(start_date, end_date)
-      @reservations << new_reservation
+      @reservations << Hotel::Reservation.new(start_date, end_date)
     end
     
     def find_available_rooms(start_date, end_date)
       @rooms.map do |room|
         room
+      end
+    end
+    
+    def list_reservations_by_date(date)
+      @reservations.map do |reservation|
+        # Currently includes end date in search, may want to alter.
+        if (reservation.dates.include? Date.parse(date)) || (reservation.end_date == Date.parse(date))
+          reservation
+        end
       end
     end
     
--- test/reservation_manager_test.rb
+++ test/reservation_manager_test.rb
@@ -24,13 +24,14 @@ describe "Reservation Manager" do
   it "creates a reservation for a given date range" do
     @hotel.create_reservation('2019-10-31', '2019-11-04')
     new_reservation = @hotel.reservations[0]
-
+    
     expect(new_reservation.start_date.to_s).must_equal '2019-10-31'
     expect(new_reservation.end_date.to_s).must_equal '2019-11-04'
   end
   
   it "returns a list of available rooms" do
     new_reservation = @hotel.create_reservation('2019-10-01', '2019-10-11')
+    expect(new_reservation[0].start_date.to_s).must_equal '2019-10-01'
     available_rooms = @hotel.find_available_rooms('2019-10-03', '2019-10-11')
     # wave 1: all rooms are available
     # wave 2: only some rooms are available, must_equal 20 is no longer valid
@@ -40,6 +41,14 @@ describe "Reservation Manager" do
   it "returns a list of reservations for a given date" do
     # expect reservation_manager.list_reservations_by_date(whatever date)
     # will return the reservations whose @dates includes whatever date
+    @hotel.create_reservation('2019-10-31', '2019-11-04')
+    @hotel.create_reservation('2019-10-29', '2019-10-31')
+    
+    reservations_by_date = @hotel.list_reservations_by_date('2019-10-31')
+    
+    expect(reservations_by_date).must_be_instance_of Array
+    expect(reservations_by_date[0].start_date.to_s).must_equal '2019-10-31'
+    expect(reservations_by_date[1].start_date.to_s).must_equal '2019-10-29'
   end
   
   
