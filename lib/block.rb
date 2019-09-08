3b7a22a887dbe09756f9178aca319798c7e4ff77
Kelsey Krippaehne <kelseykripp@gmail.com>
2019-09-07 18:45:44 -0700

    Added id to Reservations and Blocks

--- lib/block.rb
+++ lib/block.rb
@@ -3,14 +3,19 @@ module Hotel
   class Block < Reservation
     
     attr_reader :start_date, :end_date
-    attr_accessor :rooms, :dates, :discount, :reserved_rooms, :reservations
+    attr_accessor :id, :rooms, :dates, :discount, :reserved_rooms, :reservations
     
     def initialize(start_date, end_date, room, discount: 0.80)
       super(start_date, end_date, room)
+      @id = create_block_id
       @rooms = room
       @discount = discount
       @reserved_rooms = []
       @reservations = []
+    end
+    
+    def create_block_id
+      "#{@start_date.year % 100}#{@start_date.month}#{@start_date.day}#{rand(9999)}B"
     end
     
     def add_reservation_to_block(reservation)
--- lib/reservation.rb
+++ lib/reservation.rb
@@ -6,13 +6,18 @@ module Hotel
   class Reservation
     
     attr_reader :start_date, :end_date
-    attr_accessor :room, :dates
+    attr_accessor :id, :room, :dates
     
     def initialize(start_date, end_date, room)
       @start_date = start_date
       @end_date = end_date
       @room = room
+      @id = create_id
       @dates = get_all_dates
+    end
+    
+    def create_id
+      "#{@start_date.year % 100}#{@start_date.month}#{@start_date.day}#{rand(9999)}"
     end
     
     def get_all_dates
